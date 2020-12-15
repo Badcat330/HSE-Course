dd15d236f24a416cfd78a0148335f3fdf309c0c7
badcat330 <alexglushko@gmail.com>
2020-12-15 23:44:53 +0300

    Add specification and some tests

--- module-1/homework/TypeList/typelist/erase.h
+++ module-1/homework/TypeList/typelist/erase.h
@@ -4,3 +4,23 @@
 
 template<typename TList, typename TargetType>
 struct Erase;
+
+template<typename Head, typename Tail, typename TargetType>
+struct Erase<TypeList<Head, Tail>, TargetType> {
+    using NewTypeList = TypeList<Head, typename Erase<Tail, TargetType>::NewTypeList>;
+};
+
+template<typename Head, typename Tail, typename TargetType>
+struct Erase<TypeList<TargetType, TypeList<Head, Tail>>, TargetType> {
+    using NewTypeList = TypeList<Head, Tail>;
+};
+
+template<typename TargetType>
+struct Erase<TypeList<TargetType, NullType>, TargetType> {
+    using NewTypeList = NullType;
+};
+
+template<typename TargetType>
+struct Erase<NullType, TargetType> {
+    using NewTypeList = NullType;
+};
--- module-1/homework/TypeList/typelist/length.h
+++ module-1/homework/TypeList/typelist/length.h
@@ -4,3 +4,13 @@
 
 template<typename TList> 
 struct Length;
+
+template<typename Head, typename Tail>
+struct Length<TypeList<Head, Tail>> {
+    static const int length = 1 + Length<Tail>::length;
+};
+
+template<>
+struct Length<NullType> {
+    static const int length = 0;
+};
