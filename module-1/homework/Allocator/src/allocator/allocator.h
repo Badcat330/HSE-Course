5d644e025aa401037918eb1c8449c7787f6f4844
Alex <alextvrus@gmail.com>
2021-06-04 12:52:02 +0300

    Add some steps to allocator

--- module-1/homework/Allocator/src/allocator/allocator.h
+++ module-1/homework/Allocator/src/allocator/allocator.h
@@ -6,11 +6,20 @@ class CustomAllocator {
 public:
     template <typename U>
     struct rebind {  // NOLINT
-        // Your code goes here
+        using other = CustomAllocator<U>;
     };
 
     using value_type = T;
-    // Your code goes here
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using size_type = std::size_t;
+    using pointer_difference = std::ptrdiff_t;
+    using propagate_on_container_copy_assignment = std::false_type;
+    using propagate_on_container_move_assignment = std::false_type;
+    using propogate_on_container_swap = std::false_type;
+    using is_always_equal = std::false_type;
 
     CustomAllocator();
     CustomAllocator(const CustomAllocator& other) noexcept;
@@ -20,7 +29,7 @@ public:
     explicit CustomAllocator(const CustomAllocator<U>& other) noexcept;
 
     T* allocate(size_t n) {  // NOLINT
-        // Your code goes here        
+        // Your code goes here
     }
     void deallocate(T* p, size_t n) {  // NOLINT
         // Your code goes here
@@ -39,7 +48,7 @@ public:
     friend bool operator!=(const CustomAllocator<K>& lhs, const CustomAllocator<U>& rhs) noexcept;
 
 private:
-    // Your code goes here
+
 };
 
 template <typename T, typename U>
