d87b08f2f914fbdc8241a5c856afefad98c8e63a
badcat330 <alexglushko@gmail.com>
2021-06-07 15:04:31 +0300

    Implement optional

--- module-1/homework/Optional/optional.h
+++ module-1/homework/Optional/optional.h
@@ -1,64 +1,185 @@
 #include <cstdlib>
 #include <type_traits>
+#include <optional>
 
 #pragma once
 
 namespace task {
 
 struct NullOpt {
-    // Your code goes here;
+    explicit constexpr NullOpt(int) {}
 };
 
-constexpr NullOpt kNullOpt = // Your code goes here;
+constexpr NullOpt kNullOpt = NullOpt(0);
 
 struct InPlace {
-    // Your code goes here;
+    explicit InPlace() = default;
 };
 
-constexpr InPlace kInPlace =  //Your code goes here;
+constexpr InPlace kInPlace = InPlace();
 
-template <typename T>
-class Optional : public // Your code goes here; {
-public:
-    using value_type =  // Your code goes here;
+template<typename T, bool= std::is_trivially_destructible_v<T>>
+class OptionalDestructBase;
 
-    constexpr Optional() noexcept;
+template<typename T>
+class OptionalDestructBase<T, false> {
+  public:
+    using value_type = T;
 
-    template <typename U = value_type>
-    constexpr explicit Optional(U&& value);
+    ~OptionalDestructBase() {
+        if (engaged_) {
+            val_.~value_type();
+        }
+    }
 
-    constexpr explicit Optional(NullOpt) noexcept;
+    constexpr OptionalDestructBase() noexcept: null_state_(), engaged_(false) {}
 
-    template <typename... Args>
-    constexpr explicit Optional(InPlace, Args&&... args);
+    constexpr OptionalDestructBase(NullOpt) noexcept: null_state_(), engaged_(false) {}
 
-    Optional& operator=(NullOpt) noexcept;
+    template<typename... Args>
+    constexpr OptionalDestructBase(InPlace, Args&& ... args): val_(std::forward<Args>(args)...), engaged_(true) {}
 
-    template <typename U = T>
-    Optional& operator=(U&& value);
+    template<typename U = T>
+    constexpr OptionalDestructBase(U&& value): val_(std::forward<U>(value)), engaged_(true) {}
 
-    void Reset() noexcept;
+  protected:
+    void Reset() noexcept {
+        if (engaged_) {
+            val_.~value_type();
+            engaged_ = false;
+        }
+    }
 
-    template <typename U>
-    constexpr T ValueOr(U&& default_value) const&;
+    template<typename U = T>
+    void Set(U&& value) {
+        if (engaged_) {
+            val_.~value_type();
+        }
+        val_ = std::forward<U>(value);
+        engaged_ = true;
+    }
 
-    template <typename U>
-    constexpr T ValueOr(U&& default_value) &&;
+    union {
+        char null_state_;
+        value_type val_;
+    };
 
-    constexpr bool HasValue() const noexcept;
+    bool engaged_;
+};
 
-    constexpr explicit operator bool() const noexcept;
+template<typename T>
+class OptionalDestructBase<T, true> {
+  public:
+    using value_type = T;
 
-    constexpr std::add_pointer_t<const value_type> operator->() const;
+    constexpr OptionalDestructBase() noexcept: null_state_(), engaged_(false) {}
 
-    constexpr std::add_pointer_t<value_type> operator->();
+    constexpr OptionalDestructBase(NullOpt) noexcept: null_state_(), engaged_(false) {}
 
-    constexpr const value_type& operator*() const&;
+    template<typename... Args>
+    constexpr OptionalDestructBase(InPlace, Args&& ... args): val_(std::forward<Args>(args)...), engaged_(true) {}
 
-    constexpr value_type& operator*() &;
+    template<typename U = T>
+    constexpr OptionalDestructBase(U&& value): val_(std::forward<U>(value)), engaged_(true) {}
 
-    constexpr const value_type&& operator*() const&&;
+  protected:
+    void Reset() {
+        if (engaged_) {
+            engaged_ = false;
+        }
+    }
 
-    constexpr value_type&& operator*() &&;
+    template<typename U = T>
+    void Set(U&& value) {
+        val_ = std::forward<U>(value);
+        engaged_ = true;
+    }
+
+    union {
+        char null_state_;
+        value_type val_;
+    };
+
+    bool engaged_;
+};
+
+template<typename T>
+class Optional : public OptionalDestructBase<T> {
+  public:
+    using base = OptionalDestructBase<T>;
+    using value_type = T;
+
+    constexpr Optional() noexcept = default;
+
+    template<typename U = value_type>
+    constexpr explicit Optional(U&& value) : base(std::forward<U>(value)) {}
+
+    constexpr explicit Optional(NullOpt) noexcept {}
+
+    template<typename... Args>
+    constexpr explicit Optional(InPlace, Args&& ... args): base(kInPlace, std::forward<Args>(args)...) {}
+
+    Optional& operator=(NullOpt) noexcept {
+        base::Reset();
+        return *this;
+    }
+
+    template<typename U = T>
+    Optional& operator=(U&& value) {
+        base::Set(std::forward<U>(value));
+        return *this;
+    }
+
+    void Reset() noexcept {
+        base::Reset();
+    }
+
+    template<typename U>
+    constexpr T ValueOr(U&& default_value) const& {
+        if (base::engaged_) {
+            return base::val_;
+        }
+        return default_value;
+    }
+
+    template<typename U>
+    constexpr T ValueOr(U&& default_value)&& {
+        if (base::engaged_) {
+            return base::val_;
+        }
+        return default_value;
+    }
+
+    constexpr bool HasValue() const noexcept {
+        return base::engaged_;
+    }
+
+    constexpr explicit operator bool() const noexcept {
+        return HasValue();
+    }
+
+    constexpr std::add_pointer_t<const value_type> operator->() const {
+        return base::val_;
+    }
+
+    constexpr std::add_pointer_t<value_type> operator->() {
+        return &(base::val_);
+    }
+
+    constexpr const value_type& operator*() const& {
+        return base::val_;
+    }
+
+    constexpr value_type& operator*()& {
+        return base::val_;
+    }
+
+    constexpr const value_type&& operator*() const&& {
+        return std::move(base::val_);
+    }
+
+    constexpr value_type&& operator*()&& {
+        return std::move(base::val_);
+    }
 };
 }  // namespace task
