636896395a10fb630c4cc7a2b2c83e94644061be
badcat330 <alexglushko@gmail.com>
2021-06-09 22:11:53 +0300

    Implement Variant

--- module-1/homework/Variant/variant.h
+++ module-1/homework/Variant/variant.h
@@ -5,6 +5,150 @@
 
 namespace task {
 
+//----------------Helper structures----------------
+
+template <std::size_t Index>
+struct InPlaceIndex {
+  explicit InPlaceIndex() = default;
+};
+
+template <std::size_t Index>
+constexpr InPlaceIndex<Index> kInPlaceIndex{};
+
+template <std::size_t Index, typename... Types>
+union Union;
+
+template <std::size_t Index>
+union Union<Index> {};
+
+template <std::size_t Index, typename T, typename... Types>
+union Union<Index, T, Types...> {
+public:
+  T head;
+  Union<Index + 1, Types...> tail;
+
+  Union() : tail() {
+    if (!std::is_trivially_constructible_v<T>) {
+      new(&head) T();
+    }
+  }
+
+  ~Union() {
+    if (!std::is_trivially_destructible_v<T>) {
+      head.~T();
+    }
+  }
+
+  friend struct AssignUnion;
+  friend struct AccessUnion;
+};
+
+struct AssignUnion {
+  template <std::size_t Index, typename U, std::size_t UnionIndex, typename Head, typename... Tail>
+  static void Set(U&& value, InPlaceIndex<0>, Union<UnionIndex, Head, Tail...>& u) {
+    u.head = value;
+  }
+
+  template <std::size_t Index, typename U, std::size_t UnionIndex, typename Head, typename... Tail>
+  static void Set(U&& value, InPlaceIndex<Index>, Union<UnionIndex, Head, Tail...>& u) {
+    AssignUnion::Set<Index - 1>(std::forward<U>(value), kInPlaceIndex<Index - 1>, u.tail);
+  }
+};
+
+struct AccessUnion {
+  template <typename U>
+  static constexpr auto&& Get(U&& value, InPlaceIndex<0>) {
+    return std::forward<U>(value).head;
+  }
+
+  template <typename U, std::size_t Index>
+  static constexpr auto&& Get(U&& value, InPlaceIndex<Index>) {
+    return Get(std::forward<U>(value).tail, kInPlaceIndex<Index - 1>);
+  }
+};
+
+struct AccessVariant {
+  template <std::size_t Index, typename T>
+  static constexpr auto&& Get(T&& value) {
+    return AccessUnion::Get(std::forward<T>(value).data_, kInPlaceIndex<Index>);
+  }
+};
+
+//----------------Type list----------------
+
+template <typename Head, typename... Tail>
+struct TypeList {
+  using head = Head;
+  using tail = TypeList<Tail...>;
+};
+
+template <unsigned int index, class TList>
+struct TypeAt;
+
+template <typename Head, typename... Tail>
+struct TypeAt<0, TypeList<Head, Tail...>> {
+  using result = Head;
+};
+
+template <unsigned int index, typename Head, typename... Tail>
+struct TypeAt<index, TypeList<Head, Tail...>> {
+  using result = typename TypeAt<index - 1, TypeList<Tail...>>::result;
+};
+
+//----------------Find Type----------------
+
+const static std::size_t kNotFounded = -1;
+const static std::size_t kAmbiguity = kNotFounded - 1;
+
+template <std::size_t SizeOfFounded>
+constexpr std::size_t FindCondition(std::size_t i, std::size_t res, const bool (&founded)[SizeOfFounded],
+                                    const bool (&convertible)[SizeOfFounded]) {
+  if (res == kNotFounded && (founded[i] || convertible[i])) {
+    return i;
+  }
+
+  if(res == kAmbiguity) {
+    return res;
+  }
+
+  if (founded[i]) {
+    if(!founded[res] && convertible[res]) {
+      return i;
+    }
+
+    return kAmbiguity;
+  }
+
+  return res;
+}
+
+template <std::size_t SizeOfFounded>
+constexpr std::size_t FindPos(std::size_t current_position, const bool (&founded)[SizeOfFounded],
+                              const bool (&convertible)[SizeOfFounded]) {
+  if (current_position == SizeOfFounded) {
+    return kNotFounded;
+  }
+
+  return FindCondition(current_position, FindPos(current_position + 1, founded, convertible), founded, convertible);
+}
+
+template <typename TargetType, typename... Types>
+struct FindExactlyOneChecked {
+  constexpr static bool kFounded[sizeof...(Types)] = {std::is_same<TargetType, Types>::value...};
+  constexpr static bool kFoundedConvertible[sizeof...(Types)] = {std::is_convertible<TargetType, Types>::value...};
+  constexpr static std::size_t kValue = FindPos(0, kFounded, kFoundedConvertible);
+};
+
+template <typename T>
+struct FindExactlyOneChecked<T> {
+  static_assert(!std::is_same<T, T>::value, "Type not in empty type list");
+};
+
+template <typename TargetType, typename... Types>
+struct FindExactlyOneType : public FindExactlyOneChecked<TargetType, Types...> {};
+
+//----------------Variant----------------
+
 template <typename... Types>
 class Variant;
 
@@ -16,33 +160,53 @@ using variant_alternative_t = typename VariantAlternative<Idx, T>::type;
 
 template <size_t Idx, typename... Types>
 struct VariantAlternative<Idx, Variant<Types...>> {
-  using type =  // Your code goes here
+  using type = typename TypeAt<Idx, TypeList<Types...>>::result;
 };
 
 template <typename... Types>
 class Variant {
 public:
-    // Special member functions
-    constexpr Variant() noexcept;
+  // Special member functions
+  constexpr Variant() noexcept {}
 
-    template <typename T>
-    Variant& operator=(T&& t) noexcept;
+  template <typename T, std::size_t Pos = FindExactlyOneType<T, Types...>::kValue>
+  Variant& operator=(T&& t) noexcept {
+    AssignUnion::Set<Pos>(std::forward<T>(t), kInPlaceIndex<Pos>, data_);
+    return *this;
+  }
 
 private:
-    // Your code goes here
+  Union<0, Types...> data_;
+  friend AccessVariant;
 };
+
+
+//----------------Getters----------------
+
+template <std::size_t Idx, typename T>
+auto&& GenericGet(T&& v) {
+  return AccessVariant::Get<Idx>(std::forward<T>(v));
+}
 
 // Non-member functions
 template <size_t I, typename... Types>
-constexpr const variant_alternative_t<I, Variant<Types...>>& Get(Variant<Types...>& v);
+constexpr const variant_alternative_t<I, Variant<Types...>>& Get(Variant<Types...>& v) {
+  return GenericGet<I>(v);
+}
 
 template <size_t I, typename... Types>
-constexpr variant_alternative_t<I, Variant<Types...>>&& Get(Variant<Types...>&& v);
+constexpr variant_alternative_t<I, Variant<Types...>>&& Get(Variant<Types...>&& v) {
+  return GenericGet<I>(v);
+}
 
 template <typename T, typename... Types>
-constexpr const T& Get(Variant<Types...>& v);
+constexpr const T& Get(Variant<Types...>& v) {
+  return GenericGet<FindExactlyOneType<T, Types...>::kValue>(v);
+}
 
 template <typename T, typename... Types>
-constexpr T&& Get(Variant<Types...>&& v);
+constexpr T&& Get(Variant<Types...>&& v) {
+  return GenericGet<FindExactlyOneType<T, Types...>::kValue>(v);
+}
 
 };  // namespace task
