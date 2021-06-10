37ba3a962d19302049be7790d8480ef872275515
Alex <alextvrus@gmail.com>
2021-06-10 19:56:21 +0300

    Add SharedPointer implementation

--- module-1/homework/SharedPointer/src/control/control.h
+++ module-1/homework/SharedPointer/src/control/control.h
@@ -4,25 +4,101 @@
 
 class SharedCount {
 public:
-    // Your code goes here...
+    explicit SharedCount(std::size_t refs = 0) noexcept : shared_owners_(refs) {
+    }
+
+    void AddShared() noexcept {
+        ++shared_owners_;
+    }
+
+    bool ReleaseShared() noexcept {
+        if (--shared_owners_ == -1) {
+            OnZeroShared();
+            return true;
+        }
+
+        return false;
+    }
+
+    std::size_t UseCount() const noexcept {
+        return shared_owners_.load() + 1;
+    }
+
+    virtual void OnZeroShared() = 0;
 
 protected:
-    // Your code goes here...
+    std::atomic<std::size_t> shared_owners_;
+    virtual ~SharedCount() {
+    }
 };
 
 class SharedWeakCount : public SharedCount {
 public:
-    // Your code goes here...
+    explicit SharedWeakCount(std::size_t refs = 0) noexcept
+        : SharedCount(refs), shared_weak_owners_(refs) {
+    }
+
+    void AddShared() noexcept {
+        SharedCount::AddShared();
+    }
+
+    void AddWeak() noexcept {
+        ++shared_weak_owners_;
+    }
+
+    void ReleaseShared() noexcept {
+        if (SharedCount::ReleaseShared()) {
+            ReleaseWeak();
+        }
+    }
+
+    void ReleaseWeak() noexcept {
+        --shared_weak_owners_;
+    }
+
+    std::size_t UseCount() const noexcept {
+        return SharedCount::UseCount();
+    }
+
+    virtual SharedWeakCount* Lock() {
+        return nullptr;
+    }
+    virtual void OnZeroSharedWeak() noexcept {};
+
+private:
+    std::atomic<std::size_t> shared_weak_owners_;
 
 protected:
-    // Your code goes here...
+    virtual ~SharedWeakCount(){};
 };
 
 template <typename T, typename Deleter>
 class ControlBlock : public SharedWeakCount {
 public:
-    // Your code goes here...
+    ControlBlock(T pointer) : pointer_(pointer) {
+    }
+
+    ControlBlock(T pointer, Deleter deleter) : pointer_(pointer), deleter_(deleter) {
+    }
+
+    ControlBlock(ControlBlock&) = delete;
+    void operator=(ControlBlock&) = delete;
+
+    void OnZeroShared() noexcept override {
+        deleter_(pointer_);
+    }
+
+    SharedWeakCount* Lock() override {
+        if (UseCount() > 0) {
+            ControlBlock* newBlock = new ControlBlock(pointer_);
+            newBlock->AddShared();
+            return newBlock;
+        }
+
+        return nullptr;
+    }
 
 private:
-    // Your code goes here...
+    T pointer_;
+    Deleter deleter_;
 };
--- module-1/homework/SharedPointer/src/shared_ptr/shared_ptr.h
+++ module-1/homework/SharedPointer/src/shared_ptr/shared_ptr.h
@@ -2,6 +2,30 @@
 
 #include "../control/control.h"
 
+// Default Deleter
+template <typename T>
+struct DefaultDeleter {
+    void operator()(T pointer) noexcept {
+        delete pointer;
+    }
+};
+
+// Remove Types struct
+template <class T>
+struct RemoveExtent {
+    using type = T;
+};
+
+template <class T>
+struct RemoveExtent<T[]> {
+    using type = T;
+};
+
+template <class T, std::size_t N>
+struct RemoveExtent<T[N]> {
+    using type = T;
+};
+
 // SharedPtr
 template <typename T>
 class WeakPtr;
@@ -9,100 +33,262 @@ class WeakPtr;
 template <typename T>
 class SharedPtr {
 public:
-    using element_type =     // Your code goes here...
+    using element_type = typename RemoveExtent<T>::type;
+    using pointer_type = element_type*;
 
     constexpr SharedPtr() noexcept = default;
-    ~SharedPtr();
+
+    ~SharedPtr() {
+        if (control_ != nullptr) {
+            control_->ReleaseShared();
+        }
+
+        pointer_ = nullptr;
+        control_ = nullptr;
+    }
 
     template <typename Y>
-    explicit SharedPtr(Y* p);
+    explicit SharedPtr(Y* p) : pointer_(p) {
+        control_ = new ControlBlock<pointer_type, DefaultDeleter<pointer_type>>(p);
+    }
 
     template <typename Y, typename Deleter>
-    SharedPtr(Y* p, Deleter deleter) noexcept;
+    SharedPtr(Y* p, Deleter deleter) noexcept : pointer_(p) {
+        control_ = new ControlBlock<pointer_type, Deleter>(p, deleter);
+    }
 
-    SharedPtr(const SharedPtr& other) noexcept;
-    SharedPtr(SharedPtr&& other) noexcept;
+    SharedPtr(const SharedPtr& other) noexcept
+        : pointer_(other.pointer_), control_(other.control_) {
+        control_->AddShared();
+    }
 
-    SharedPtr& operator=(const SharedPtr& r) noexcept;
+    SharedPtr(SharedPtr&& other) noexcept : pointer_(other.pointer_), control_(other.control_) {
+        other.control_ = nullptr;
+        other.pointer_ = nullptr;
+    }
+
+    SharedPtr& operator=(const SharedPtr& r) noexcept {
+        Reset();
+        pointer_ = r.pointer_;
+        control_ = r.control_;
+        control_->AddShared();
+        return *this;
+    }
 
     template <typename Y>
-    SharedPtr& operator=(const SharedPtr<Y>& r) noexcept;
+    SharedPtr& operator=(const SharedPtr<Y>& r) noexcept {
+        if (this == r) {
+            return *this;
+        }
 
-    SharedPtr& operator=(SharedPtr&& r) noexcept;
+        Reset();
+        pointer_ = r.pointer_;
+        control_ = r.control_;
+        control_->AddShared();
+        return *this;
+    }
+
+    SharedPtr& operator=(SharedPtr&& r) noexcept {
+        Reset();
+        pointer_ = r.pointer_;
+        control_ = r.control_;
+        r.control_ = nullptr;
+        r.pointer_ = nullptr;
+        return *this;
+    }
 
     template <typename Y>
-    SharedPtr& operator=(SharedPtr<Y>&& r) noexcept;
+    SharedPtr& operator=(SharedPtr<Y>&& r) noexcept {
+        Reset();
+        pointer_ = r.pointer_;
+        control_ = r.control_;
+        r.control_ = nullptr;
+        r.pointer_ = nullptr;
+        return *this;
+    }
 
     // Modifiers
-    void Reset() noexcept;
+    void Reset() noexcept {
+        pointer_ = nullptr;
+        if (control_ != nullptr) {
+            control_->ReleaseShared();
+        }
+        control_ = nullptr;
+    }
 
     template <typename Y>
-    void Reset(Y* p) noexcept;
+    void Reset(Y* p) noexcept {
+        if (control_ != nullptr) {
+            control_->ReleaseShared();
+        }
+
+        pointer_ = p;
+        control_ = new ControlBlock<Y*, DefaultDeleter<pointer_type>>(p);
+    }
 
     template <typename Y, typename Deleter>
-    void Reset(Y* p, Deleter deleter) noexcept;
+    void Reset(Y* p, Deleter deleter) noexcept {
+        if (control_ != nullptr) {
+            control_->ReleaseShared();
+        }
 
-    void Swap(SharedPtr& other) noexcept;
+        pointer_ = p;
+        control_ = new ControlBlock<Y*, Deleter>(p, deleter);
+    }
+
+    void Swap(SharedPtr& other) noexcept {
+        std::swap(pointer_, other.pointer_);
+        std::swap(control_, other.control_);
+    }
 
     // Observers
-    T* Get() const noexcept;
-    int64_t UseCount() const noexcept;
-    T& operator*() const noexcept;
-    T* operator->() const noexcept;
-    element_type& operator[](std::ptrdiff_t idx) const;
-    explicit operator bool() const noexcept;
+    T* Get() const noexcept {
+        return pointer_;
+    }
+
+    int64_t UseCount() const noexcept {
+        if (control_ != nullptr) {
+            return control_->UseCount();
+        }
+
+        return 0;
+    }
+
+    T& operator*() const noexcept {
+        return *pointer_;
+    }
+
+    T* operator->() const noexcept {
+        return pointer_;
+    }
+
+    element_type& operator[](std::ptrdiff_t idx) const {
+        return pointer_[idx];
+    }
+
+    explicit operator bool() const noexcept {
+        return control_ != nullptr && pointer_ != nullptr;
+    }
 
     template <typename U>
     friend class WeakPtr;
 
 private:
-    // Your code goes here...
+    pointer_type pointer_ = nullptr;
+    SharedWeakCount* control_ = nullptr;
 };
 
-
-// MakeShared
-// Your code goes here...
-// MakeShared
-
-// SharedPtr
-// Your code goes here...
-// SharedPtr
+template <typename T, typename... Args>
+SharedPtr<T> MakeShared(Args&&... args) {
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
 
 // WeakPtr
 template <typename T>
 class WeakPtr {
 
 public:
-    using element_type =     // Your code goes here...
+    using element_type = typename RemoveExtent<T>::type;
+    using poiter_type = element_type*;
 
     // Special-member functions
     constexpr WeakPtr() noexcept = default;
-    template <typename Y>
-    explicit WeakPtr(const SharedPtr<Y>& other);
-    WeakPtr(const WeakPtr& other) noexcept;
-    WeakPtr(WeakPtr&& other) noexcept;
-    template <typename Y>
-    WeakPtr& operator=(const SharedPtr<Y>& other);
-    WeakPtr& operator=(const WeakPtr& other) noexcept;
-    WeakPtr& operator=(WeakPtr&& other) noexcept;
 
-    ~WeakPtr();
+    template <typename Y>
+    explicit WeakPtr(const SharedPtr<Y>& other)
+        : pointer_(other.pointer_), control_(other.control_) {
+        control_->AddWeak();
+    }
+
+    WeakPtr(const WeakPtr& other) noexcept : pointer_(other.pointer_), control_(other.control_) {
+        control_->AddWeak();
+    }
+
+    WeakPtr(WeakPtr&& other) noexcept : pointer_(other.pointer_), control_(other.control_) {
+        other.pointer_ = nullptr;
+        other.control_ = nullptr;
+    }
+
+    template <typename Y>
+    WeakPtr& operator=(const SharedPtr<Y>& other) {
+        Reset();
+
+        pointer_ = other.pointer_;
+        control_ = other.control_;
+
+        if (control_ != nullptr) {
+            control_->AddWeak();
+        }
+
+        return *this;
+    }
+
+    WeakPtr& operator=(const WeakPtr& other) noexcept {
+        if (this == other) {
+            return *this;
+        }
+
+        Reset();
+        pointer_ = other.pointer_;
+        control_ = other.control_;
+
+        if (control_ != nullptr) {
+            control_->AddWeak();
+        }
+
+        return *this;
+    }
+
+    WeakPtr& operator=(WeakPtr&& other) noexcept {
+        Reset();
+        pointer_ = other.pointer_;
+        control_ = other.control_;
+        other.pointer_ = nullptr;
+        other.control_ = nullptr;
+
+        return *this;
+    }
+
+    ~WeakPtr() {
+        if (control_ != nullptr) {
+            control_->ReleaseWeak();
+        }
+    }
 
     // Modifiers
-    void Reset() noexcept;
-    void Swap(WeakPtr<T>& other) noexcept;
+    void Reset() noexcept {
+        pointer_ = nullptr;
+        if (control_ != nullptr) {
+            control_->ReleaseWeak();
+        }
+        control_ = nullptr;
+    }
+
+    void Swap(WeakPtr<T>& other) noexcept {
+        std::swap(pointer_, other.pointer_);
+        std::swap(control_, other.control_);
+    }
 
     // Observers
-    bool Expired() noexcept;
-    SharedPtr<T> Lock() const noexcept;
+    bool Expired() noexcept {
+        return control_ == nullptr || control_->UseCount() == 0;
+    }
+
+    SharedPtr<T> Lock() const noexcept {
+        SharedPtr<element_type> shared_ptr;
+        shared_ptr.pointer_ = pointer_;
+
+        if (control_ != nullptr) {
+            shared_ptr.control_ = control_->Lock();
+        }
+
+        return shared_ptr;
+    }
 
     template <typename U>
     friend class SharedPtr;
 
 public:
-    // Your code goes here...
+    poiter_type pointer_ = nullptr;
+    SharedWeakCount* control_ = nullptr;
 };
-
-// WeakPtr
-// Your code goes here...
-// WeakPtr
