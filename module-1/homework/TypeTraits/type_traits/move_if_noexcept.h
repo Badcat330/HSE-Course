cbb53ef25c596d732ad0d5fb091fc12bfb8a4e6c
badcat330 <alexglushko@gmail.com>
2021-06-13 18:41:18 +0300

    Add move_if_noexcept.h implementation

--- module-1/homework/TypeTraits/type_traits/move_if_noexcept.h
+++ module-1/homework/TypeTraits/type_traits/move_if_noexcept.h
@@ -9,21 +9,24 @@
 
 template <bool condition, typename T, typename F>
 struct Conditional {
-    // Your code goes here
+  using type = T;
 };
 
-// Conditional - partial specialization
-// Your code goes here
-// Conditional - partial specialization
+template <typename T, typename F>
+struct Conditional<false, T, F> {
+  using type = F;
+};
 
 template <typename T, typename F>
 struct Conditional<true, T, F> {
-    using type = T;
+  using type = T;
 };
 
 template <bool condition, typename T, typename F>
-using conditional_v = // Your code goes here
+using conditional_v = typename Conditional<condition, T, F>::type;
 
-// MoveIfNoExcept
-// Your code goes here
-// MoveIfNoExcept
+template <typename T>
+conditional_v<!IsNoThrowMoveConstructible<T>::value && IsCopyConstructible<T>::value, const T&, T&&>
+MoveIfNoExcept(T& x) {
+  return std::move(x);
+}
